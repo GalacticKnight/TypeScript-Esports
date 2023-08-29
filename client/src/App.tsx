e573a2775b7eefdac5a7ba35d1a04554872c6a84
Vincent Lau <35178680+GalacticKnight@users.noreply.github.com>
2023-08-29 13:34:35 -0400

    retake

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,35 @@
-import HomePage from "./pages/HomePage";
-import './App.css';
+// import HomePage from "./pages/HomePage";
+// import './App.css';
+
+// function App() {
+//   return (
+//     <>
+//       <div>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default App
+
+import HomePage from './pages/HomePage';
+import History from './pages/History';
+
+import Member from './pages/Members';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <div>
-      <HomePage/>
-      </div>
+      <BrowserRouter> 
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/history' element={<History/>}/>
+          <Route path='/member' element={<Member/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
